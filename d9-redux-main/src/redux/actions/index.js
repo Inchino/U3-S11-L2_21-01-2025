4c60703e204016cfab8e55ba5c49af3382732a89
Inchino <alessandro.incalza22@gmail.com>
2025-01-21 17:22:20 +0100

    Correzione punto 1

--- d9-redux-main/src/redux/actions/index.js
+++ d9-redux-main/src/redux/actions/index.js
@@ -1,16 +1,16 @@
-export const ADD_TO_FAVOURITES = 'ADD_TO_FAVOURITES'
-export const REMOVE_FROM_FAVOURITES = 'REMOVE_FROM_FAVOURITES'
+export const ADD_TO_FAVOURITE = 'ADD_TO_FAVOURITE'
+export const REMOVE_FROM_FAVOURITE = 'REMOVE_FROM_FAVOURITE'
 
 export const addToFavouritesAction = (job) => {
     return {
-      type: ADD_TO_FAVOURITES,
+      type: ADD_TO_FAVOURITE,
       payload: job,
     }
   }
 
   export const removeFromFavouritesAction = (i) => {
     return {
-      type: REMOVE_FROM_FAVOURITES,
+      type: REMOVE_FROM_FAVOURITE,
       payload: i,
     }
   }
